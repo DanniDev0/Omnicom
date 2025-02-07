b8194ee8e3e0886a26de1a1edb67e139fb733305
XLorian <danielperezbedoya03@gmail.com>
2025-02-07 16:13:48 -0500

    eliminacion de componentes innecesarios

--- src/pages/Contact_us.jsx
+++ src/pages/Contact_us.jsx
@@ -3,11 +3,6 @@ import Title from '../components/Title';
 import '../assets/style/Contact_us.css';
 import MapComponent from '../components/MapComponent';
 import ContactFormComponent from '../components/ContactFormComponent';
-import InfoCardComponent from '../components/InfoCardComponent';
-import addressIcon from '../assets/img/addressIcon.png';
-import phoneIcon from '../assets/img/phoneIcon.png';
-import emailIcon from '../assets/img/emailIcon.png';
-import hoursIcon from '../assets/img/opening-hoursIcon.png';
 
 export const Contact_us = () => {
     return (
@@ -17,14 +12,6 @@ export const Contact_us = () => {
                 <div className="container">
                     <MapComponent />
                     <ContactFormComponent />
-                </div>
-            </section>
-            <section>
-                <div className="info-company">
-                    <InfoCardComponent icon={addressIcon} title="Address" content="PO BOX 2734, WOODBRIDGE, VA, 22195" />
-                    <InfoCardComponent icon={phoneIcon} title="Phone" content="+1 5712788712" />
-                    <InfoCardComponent icon={emailIcon} title="Email" content="info@omnicomcr.com" />
-                    <InfoCardComponent icon={hoursIcon} title="Opening Hours" content="Monday - Friday: 9:00 AM - 6:00 PM" />
                 </div>
             </section>
         </div>
