22695702d3254eb6c3e17678c5d0b3c44ee827f8
XLorian <danielperezbedoya03@gmail.com>
2025-05-22 15:21:01 -0500

    Rediseño del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../assets/style/Header.css';
 import { Link } from 'react-router-dom';
-import logo from '../assets/img/logo.png';
+import logo from '../assets/img/logo.webp';
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
