596b3b7c53e65c5b6b3ece3f80ee0124f4c1799f
XLorian <danielperezbedoya03@gmail.com>
2025-05-24 16:09:35 -0500

    Mejoras footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import '../assets/style/Footer.css';
-import Facebook from '../assets/img/Facebook.png';
-import Instagram from '../assets/img/Instagram.png';
-import X from '../assets/img/X.png';
-import Location from '../assets/img/Location.png';
-import Email from '../assets/img/Email.png';
-import Phone from '../assets/img/Phone.png';
-import NMSDC from '../assets/img/Certificate_NMSDC.png';
-import SWaM from '../assets/img/Certificate_SWaM.png';
+import React from "react";
+import "../assets/style/Footer.css";
+import Facebook from "../assets/img/Facebook.png";
+import Instagram from "../assets/img/Instagram.png";
+import X from "../assets/img/X.png";
+import Location from "../assets/img/Location.png";
+import Email from "../assets/img/Email.png";
+import Phone from "../assets/img/Phone.png";
+import NMSDC from "../assets/img/Certificate_NMSDC.png";
+import SWaM from "../assets/img/Certificate_SWaM.png";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-
         <div className="footer-column">
           <h3>Contact</h3>
           <ul className="footer-list">
@@ -41,10 +40,18 @@ export const Footer = () => {
         <div className="footer-column">
           <h3>Follow Us</h3>
           <div className="social-links">
-            <a href="https://www.facebook.com/omnicomcr" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/omnicomcr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={Facebook} alt="Facebook" />
             </a>
-            <a href="https://www.instagram.com/omnicom_cr" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.instagram.com/omnicom_cr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={Instagram} alt="Instagram" />
             </a>
             <a href="https://x.com" target="_blank" rel="noopener noreferrer">
@@ -56,8 +63,20 @@ export const Footer = () => {
         <div className="footer-column">
           <h3>Certifications</h3>
           <div className="certifications">
-            <img src={NMSDC} alt="NMSDC Certificate" />
-            <img src={SWaM} alt="SWaM Certificate" />
+            <a
+              href="https://nmsdc.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={NMSDC} alt="NMSDC Certificate" />
+            </a>
+            <a
+              href="https://sbsd.virginia.gov/certification-division/swam/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={SWaM} alt="SWaM Certificate" />
+            </a>
           </div>
         </div>
       </div>
